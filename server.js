5224e8852aea87322a6466512da73f80b0ce2c26
kelvinocampo <kevinocampooso@gmail.com>
2025-01-13 14:01:53 -0500

    Change fixed port

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ io.on('connection', (socket) => {
 });
 
 // Iniciar el servidor
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
